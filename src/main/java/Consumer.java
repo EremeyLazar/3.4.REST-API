9036977e5e3cee73d01e5be4308143bb4c86c91d
Eremey Lazar <elazar1926@gmail.com>
2024-01-23 17:25:03 +0300

    FUNC

--- src/main/java/Consumer.java
+++ src/main/java/Consumer.java
@@ -7,31 +7,30 @@ public class Consumer {
 
     public static void main(String[] args) {
         String URL = "http://94.198.50.185:7081/api/users";
-        String URLdelete = "http://94.198.50.185:7081/api/users/{id}";
+        String URLToDelete = "http://94.198.50.185:7081/api/users/{id}";
+        User userToSave = new User(3L, "James", "Brown", (byte) 33);
+        User userToUpdate = new User(3L, "Thomas", "Shelby", (byte) 33);
 
         RestTemplate template = new RestTemplate();
-        ResponseEntity<String> forEntity = template.getForEntity(URL, String.class);
-        List<String> cookies = forEntity.getHeaders().get("Set-Cookie");
+        ResponseEntity<String> responseEntityEntity = template.getForEntity(URL, String.class);
+        List<String> cookies = responseEntityEntity.getHeaders().get("Set-Cookie");
 
         System.out.println("Received Cookies: " + cookies);
-
-        User userToSave = new User(3L, "James", "Brown", (byte) 33);
-        User userToChange = new User(3L, "Thomas", "Shelby", (byte) 33);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set("Cookie", String.join("; ", cookies));
 
         HttpEntity<User> entity = new HttpEntity<>(userToSave, headers);
-        ResponseEntity<String> responseEntity1 = template.exchange(URL, HttpMethod.POST, entity, String.class);
-        System.out.println("Response from POST: " + responseEntity1.getBody());
+        ResponseEntity<String> responseEntityPOST = template.exchange(URL, HttpMethod.POST, entity, String.class);
+        System.out.println("Response from POST: " + responseEntityPOST.getBody());
 
-        HttpEntity<User> entity2 = new HttpEntity<>(userToChange, headers);
-        ResponseEntity<String> responseEntity2 = template.exchange(URL, HttpMethod.PUT, entity2, String.class);
-        System.out.println("Response from PUT: " + responseEntity2.getBody());
+        HttpEntity<User> entity2 = new HttpEntity<>(userToUpdate, headers);
+        ResponseEntity<String> responseEntityPUT = template.exchange(URL, HttpMethod.PUT, entity2, String.class);
+        System.out.println("Response from PUT: " + responseEntityPUT.getBody());
 
         HttpEntity<User> entity3 = new HttpEntity<>(headers);
-        ResponseEntity<String> responseEntity3 = template.exchange(URLdelete, HttpMethod.DELETE, entity3, String.class, 3);
-        System.out.println("Response from DELETE: " + responseEntity3.getBody());
+        ResponseEntity<String> responseEntityDELETE = template.exchange(URLToDelete, HttpMethod.DELETE, entity3, String.class, 3);
+        System.out.println("Response from DELETE: " + responseEntityDELETE.getBody());
     }
 }
